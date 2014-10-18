d45ea8b6d5757872539ac9a5ab1fafaf355220a9
Brian Rhea <brianrhea@gmail.com>
2014-10-18 10:35:14 -0600

    files

---
+++ js/omnisearch.js
@@ -0,0 +1,118 @@
+(function ( $ ) {
+
+	$.fn.droplist = function(options) {
+
+		var settings = $.extend({
+			overflow: "left",
+            ellipsis: "&#8230;",
+            dropicon: "caret"
+        }, options );
+
+        if ( settings.dropicon !== "caret" ) {
+        	settings.dropicon = "fa " + settings.dropicon;
+        }
+
+		this.each(function(){
+			var createDropList			= false;
+			var width					= $(this).width() - 26;
+			var children				= $.makeArray($(this).find('li'));
+			var childrenWithWidths		= [];
+			var childrenWidthTotal		= 0;
+			var hiddenChildren			= [];
+
+			_.each(children, function(child){
+				childrenWidthTotal += $(child).outerWidth();
+				childrenWithWidths.push([child, $(child).outerWidth()]);
+			});
+
+			if ( settings.overflow === "left" ) {
+
+				$(this).addClass('droplist-left');
+
+				if ( childrenWidthTotal > width ) {
+					childrenWidthTotal = 0;
+					for ( var i = childrenWithWidths.length - 1; i >= 0; i-- ) {
+						childrenWidthTotal += childrenWithWidths[i][1];
+						if ( childrenWidthTotal > width ) {
+							hiddenChildren.push(childrenWithWidths[i][0]);
+							$(childrenWithWidths[i][0]).remove();
+							var n = i + 1;
+							$(childrenWithWidths[n][0]).addClass('no-before');
+							createDropList = true;
+						}
+					}
+				}
+
+				if ( createDropList === true ) {
+
+					var html = [];
+
+					if ( settings.ellipsis ) {
+						$(this).prepend('<li class=\"droplist-ellipsis\">' + settings.ellipsis + '</li>');
+					}
+
+					$(this).prepend('<li class=\"droplist-dropdown\"><a href=\"javascript:void(0);\" class=\"droplist-toggle\" data-toggle=\"droplist\"><i class=\"' + settings.dropicon + '\"></i></a><ul class=\"droplist-menu\"></ul></li>');
+
+					_.each(hiddenChildren, function(hiddenChild){
+						html.push(hiddenChild.outerHTML);
+					})
+
+					$(this).find('.droplist-menu').append( html.join('') );
+				}
+
+			} else {
+
+				$(this).addClass('droplist-right');
+
+				if ( childrenWidthTotal > width ) {
+					childrenWidthTotal = 0;
+					for ( var i = 0; i < childrenWithWidths.length; i++ ) {
+						childrenWidthTotal += childrenWithWidths[i][1];
+						if ( childrenWidthTotal > width ) {
+							hiddenChildren.push(childrenWithWidths[i][0]);
+							$(childrenWithWidths[i][0]).remove();
+							var n = i - 1;
+							$(childrenWithWidths[n][0]).addClass('no-after');
+							createDropList = true;
+						}
+					}
+				}
+
+				if ( createDropList === true ) {
+
+					var html = [];
+
+					if ( settings.ellipsis ) {
+						$(this).append('<li class=\"droplist-ellipsis\">' + settings.ellipsis + '</li>');
+					}
+
+					$(this).append('<li class=\"droplist-dropdown\"><a href=\"javascript:void(0);\" class=\"droplist-toggle\" data-toggle=\"droplist\"><i class=\"' + settings.dropicon + '\"></i></a><ul class=\"droplist-menu\"></ul></li>');
+
+					_.each(hiddenChildren, function(hiddenChild){
+						html.push(hiddenChild.outerHTML);
+					})
+
+					$(this).find('.droplist-menu').append( html.join('') );
+				}
+
+			}
+
+		});
+	};
+
+}( jQuery ));
+
+$('.droplist').on('click', '.droplist-dropdown', function(e){
+	if ( $(this).hasClass('open') ) {
+		$(this).find('.droplist-menu').fadeOut();
+		$(this).removeClass('open');
+	} else {
+		$(this).find('.droplist-menu').fadeIn();
+		$(this).addClass('open');
+		e.stopPropagation();
+	}
+});
+
+$(document).click(function(){
+	$(this).find('.droplist-menu').fadeOut();
+});
