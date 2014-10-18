b5c53dabb9c06621ce01876292a983184736b873
Brian Rhea <brianrhea@gmail.com>
2014-10-18 10:46:37 -0600

    move js

--- js/omnisearch.js
+++ js/omnisearch.js
@@ -1,118 +1,57 @@
-(function ( $ ) {
+function getIcon(word) {
+	var icon;
+	switch (word.toLowerCase()) {
+	case 'brian':
+	case 'rhea':
+	case 'john':
+		icon = 'user';
+		break;
+	case 'born':
+		icon = 'calendar';
+		break;
+	default:
+		icon = 'question';
+	}
+	return icon;
+}
 
-	$.fn.droplist = function(options) {
+$(document).ready(function() {
 
-		var settings = $.extend({
-			overflow: "left",
-            ellipsis: "&#8230;",
-            dropicon: "caret"
-        }, options );
+	$('.search-input').on('keyup', function(){
 
-        if ( settings.dropicon !== "caret" ) {
-        	settings.dropicon = "fa " + settings.dropicon;
-        }
+		var q = $(this).val();
+		var list = $('.search-results li');
 
-		this.each(function(){
-			var createDropList			= false;
-			var width					= $(this).width() - 26;
-			var children				= $.makeArray($(this).find('li'));
-			var childrenWithWidths		= [];
-			var childrenWidthTotal		= 0;
-			var hiddenChildren			= [];
+		if ( q.length > 1 ) {
 
-			_.each(children, function(child){
-				childrenWidthTotal += $(child).outerWidth();
-				childrenWithWidths.push([child, $(child).outerWidth()]);
+			_.each(list, function(li){
+				$(li).addClass('hide');
+				if ( $(li).text().toLowerCase().indexOf(q.toLowerCase()) !== -1 ) {
+					$(li).removeClass('hide');
+				}
 			});
 
-			if ( settings.overflow === "left" ) {
+			var words = q.split(' ');
+			var sentence = [];
 
-				$(this).addClass('droplist-left');
+			_.each(words, function(word, index){
 
-				if ( childrenWidthTotal > width ) {
-					childrenWidthTotal = 0;
-					for ( var i = childrenWithWidths.length - 1; i >= 0; i-- ) {
-						childrenWidthTotal += childrenWithWidths[i][1];
-						if ( childrenWidthTotal > width ) {
-							hiddenChildren.push(childrenWithWidths[i][0]);
-							$(childrenWithWidths[i][0]).remove();
-							var n = i + 1;
-							$(childrenWithWidths[n][0]).addClass('no-before');
-							createDropList = true;
-						}
-					}
-				}
+				var icon = getIcon(word);
 
-				if ( createDropList === true ) {
+				word = '<span class="span-wrapper"><i class="fa fa-' + icon + '"></i><span class="span-content">' + word + '</span></span>';
+				sentence.push(word);
 
-					var html = [];
+			});
 
-					if ( settings.ellipsis ) {
-						$(this).prepend('<li class=\"droplist-ellipsis\">' + settings.ellipsis + '</li>');
-					}
+			$('.overlay').html(sentence.join(' '));
 
-					$(this).prepend('<li class=\"droplist-dropdown\"><a href=\"javascript:void(0);\" class=\"droplist-toggle\" data-toggle=\"droplist\"><i class=\"' + settings.dropicon + '\"></i></a><ul class=\"droplist-menu\"></ul></li>');
+			$('.search-input').css('padding-left', '51px');
 
-					_.each(hiddenChildren, function(hiddenChild){
-						html.push(hiddenChild.outerHTML);
-					})
+		} else {
+			$('.overlay').html('');
+			$('.search-input').css('padding-left', '0px');
+		}
 
-					$(this).find('.droplist-menu').append( html.join('') );
-				}
+	});
 
-			} else {
-
-				$(this).addClass('droplist-right');
-
-				if ( childrenWidthTotal > width ) {
-					childrenWidthTotal = 0;
-					for ( var i = 0; i < childrenWithWidths.length; i++ ) {
-						childrenWidthTotal += childrenWithWidths[i][1];
-						if ( childrenWidthTotal > width ) {
-							hiddenChildren.push(childrenWithWidths[i][0]);
-							$(childrenWithWidths[i][0]).remove();
-							var n = i - 1;
-							$(childrenWithWidths[n][0]).addClass('no-after');
-							createDropList = true;
-						}
-					}
-				}
-
-				if ( createDropList === true ) {
-
-					var html = [];
-
-					if ( settings.ellipsis ) {
-						$(this).append('<li class=\"droplist-ellipsis\">' + settings.ellipsis + '</li>');
-					}
-
-					$(this).append('<li class=\"droplist-dropdown\"><a href=\"javascript:void(0);\" class=\"droplist-toggle\" data-toggle=\"droplist\"><i class=\"' + settings.dropicon + '\"></i></a><ul class=\"droplist-menu\"></ul></li>');
-
-					_.each(hiddenChildren, function(hiddenChild){
-						html.push(hiddenChild.outerHTML);
-					})
-
-					$(this).find('.droplist-menu').append( html.join('') );
-				}
-
-			}
-
-		});
-	};
-
-}( jQuery ));
-
-$('.droplist').on('click', '.droplist-dropdown', function(e){
-	if ( $(this).hasClass('open') ) {
-		$(this).find('.droplist-menu').fadeOut();
-		$(this).removeClass('open');
-	} else {
-		$(this).find('.droplist-menu').fadeIn();
-		$(this).addClass('open');
-		e.stopPropagation();
-	}
-});
-
-$(document).click(function(){
-	$(this).find('.droplist-menu').fadeOut();
 });
